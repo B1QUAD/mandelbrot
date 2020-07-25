865b20249f9123c2c160a3456276cc4641d6ab5c
B1QUAD <accntmail.745@gmail.com>
2020-07-24 22:45:29 -0500

    Removed large image files

--- src/image.java
+++ src/image.java
@@ -38,38 +38,4 @@ public class image {
 			System.out.println("Error: " + e.toString());
 		}
 	}
-
-	public static void main(String args[]) throws IOException {
-		//image dimension
-		int width = 1920;
-		int height = 1920;
-		//create buffered image object img
-		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		//file object
-		File f;
-		//create random image pixel by pixel
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-//         int a = (int)(Math.random()*256); //alpha
-				int a = 255;
-//				int r = (int) (Math.random() * 256); //red
-//				int g = (int) (Math.random() * 256); //green
-//				int b = (int) (Math.random() * 256); //blue
-				int r = x * (int) Math.sin(y % (y + 1));
-				int g = x % (y + 1);
-				int b = x + y;
-				int p = (a << 24) | (r << 16) | (g << 8) | b; //pixel
-				//System.out.println("R: " + r + " G: " + g + " B: " + b +" A: " + a);
-				img.setRGB(x, y, p);
-			}
-		}
-
-		//write image
-		try {
-			f = new File("Output.png");
-			ImageIO.write(img, "png", f);
-		} catch (IOException e) {
-			System.out.println("Error: " + e.toString());
-		}
-	}
 }
